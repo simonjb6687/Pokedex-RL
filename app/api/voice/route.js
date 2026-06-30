c6e4231020f43a9d2add989be3f7196d3e4de0e2
simonjb6687 <78qzjpv4pb@privaterelay.appleid.com>
2026-07-01 04:11:34 +1000

    Fix missing comma in cloudinary config (build fix)

--- app/api/voice/route.js
+++ app/api/voice/route.js
@@ -5,7 +5,7 @@ import { v2 as cloudinary } from 'cloudinary';
 export const dynamic = 'force-dynamic';
 
 cloudinary.config({
-	cloud_name: process.env.CLOUDINARY_NAME
+	cloud_name: process.env.CLOUDINARY_NAME,
 	api_key: process.env.CLOUDINARY_KEY,
 	api_secret: process.env.CLOUDINARY_SECRET
 });
