9414f01c7bc12b63fbda480920fd8ab90ca92827
simonjb6687 <78qzjpv4pb@privaterelay.appleid.com>
2026-06-30 17:12:04 +1000

    Update withUser for MongoDB

--- app/api/withUser.js
+++ app/api/withUser.js
@@ -1,13 +1,17 @@
-import db from "@/app/db";
+import getDb from "@/app/db";
 
 export const withUser = handler => async (req, res) => {
+	const db = await getDb();
+	if (!db) {
+		return await handler(req, res);
+	}
 	const collection = db.collection('users');
 	const user = await collection.findOne({
 		providerAccountId: req.token.sub,
 	});
-	if(!user){
-		return await handler(req, res)
+	if (!user) {
+		return await handler(req, res);
 	}
 	req.user = user;
-	return await handler(req, res)
+	return await handler(req, res);
 }
